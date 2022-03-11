bb83fea6080d36689cdc99d3f5a28020a2ff8e93
Vitor Henrique <vitor.msouza@b2wdigital.com>
2022-03-11 14:59:28 -0300

    feat: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,18 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.scss'],
     alias: {
         '@': path.join(__dirname, 'src')
     }
