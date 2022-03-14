499e5785405a1943ae7fdc116b34dbc517b64250
Vitor Henrique <vitor.msouza@b2wdigital.com>
2022-03-14 04:32:32 -0300

    feat: create confirmation modal

---
+++ src/presentation/components/ConfirmationModal/index.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import Button from "../Button"
+import Modal from "../Modal"
+import Styles from './styles.scss'
+import { Cancel, Confirm } from "@/presentation/icons"
+
+type Props = {
+    title: string
+    text: string
+    isOpen: boolean
+    onClose: () => void
+    onConfirm: () => void
+}
+
+const ConfirmationModal: React.FC<Props> = ({ 
+    isOpen, 
+    onClose, 
+    onConfirm, 
+    text, 
+    title 
+}) => {
+    return (
+        <Modal isOpen={isOpen} title={title} onClose={onClose}>
+            <p>{ text }</p>
+            <div className={Styles.actionsContainer}>
+                <Button icon={Cancel} onClick={onClose}>cancel</Button>
+                <Button icon={Confirm} onClick={onConfirm}>confirm</Button>
+            </div>
+        </Modal>
+    )
+} 
+
+export default ConfirmationModal
--- src/presentation/icons/index.ts
+++ src/presentation/icons/index.ts
@@ -5,3 +5,5 @@ export { FiTrash as Trash } from 'react-icons/fi'
 export { FiX as Close } from 'react-icons/fi'
 export { FiSave as Save } from 'react-icons/fi'
 export { FiXCircle as Cancel } from 'react-icons/fi'
+export { FiCheckCircle as Confirm } from 'react-icons/fi'
+
