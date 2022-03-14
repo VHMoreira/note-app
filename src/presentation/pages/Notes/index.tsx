e14a57df1a349a6cf519ecb50ba83494854e70fe
Vitor Henrique <vitor.msouza@b2wdigital.com>
2022-03-14 16:06:12 -0300

    feat: finish notes page layout

--- src/presentation/pages/Notes/index.tsx
+++ src/presentation/pages/Notes/index.tsx
@@ -1,7 +1,50 @@
 import React from 'react'
+import Style from './style.scss'
+import { Button, CreateNoteModal, NoteList } from '@/presentation/components'
+import { Plus } from '@/presentation/icons'
+import { Note } from '@/domain/models'
+import { useToggle } from '@/presentation/hooks'
+
+const notesMock: Note[] = [
+    {
+        id: 1,
+        title: 'Teste note 1',
+        itens: [
+            {id:1, isDone: false, text: "Jogar Bola"},
+            {id:2, isDone: true, text: "Estudar"}
+        ]
+    },
+    {
+        id: 2,
+        title: 'Teste note 1',
+        itens: [
+            {id:1, isDone: false, text: "Jogar Bola"},
+            {id:2, isDone: true, text: "Estudar"}
+        ]
+    },
+    {
+        id: 3,
+        title: 'Teste note 1',
+        itens: [
+            {id:1, isDone: false, text: "Jogar Bola"},
+            {id:2, isDone: true, text: "Estudar"}
+        ]
+    }
+] 
 
 const Notes: React.FC = () => {
-    return <h1>Notes</h1>
+    const { isActive, enable, disable } = useToggle()
+    return (
+        <section className={Style.notesContainer}>
+            <article className={Style.notesListWrapper}>
+                <div className={Style.notesControllers}>
+                    <Button icon={Plus} onClick={enable}>new note</Button>
+                </div>
+                <NoteList notes={notesMock} readOnly/>
+            </article>
+            <CreateNoteModal isOpen={isActive} onClose={disable}/>
+        </section>
+    )
 }
 
 export default Notes
