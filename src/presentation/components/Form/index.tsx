6cfc16228495d3cc856f2a3bab5b7738c1afb9e5
Vitor Henrique <vitor.msouza@b2wdigital.com>
2022-03-15 15:06:20 -0300

    feat: create edit note page layout

---
+++ src/presentation/components/Form/index.tsx
@@ -0,0 +1,74 @@
+import React from "react"
+import Button from "../Button"
+import { Cancel, Plus, Save, Trash } from "@/presentation/icons"
+import Styles from './styles.scss'
+
+type ItensChangeHandler = (event: React.ChangeEvent<HTMLInputElement>, index: number) => void
+
+type Props = {
+    titleValue: string
+    onChangeTitleValue: React.ChangeEventHandler<HTMLInputElement>
+    itensValue: string[]
+    onAddNewItem: () => void
+    onChangeItemValue: ItensChangeHandler
+    onDeleteItem: (index: number) => void
+    canDeleteItem: boolean
+    onCancel: () => void
+    onSaveNote: () => void
+}
+
+const Form: React.FC<Props> = ({
+    titleValue,
+    onChangeTitleValue,
+    itensValue,
+    onAddNewItem,
+    onChangeItemValue,
+    onDeleteItem,
+    canDeleteItem,
+    onCancel,
+    onSaveNote
+}) => {
+    return (
+        <form>
+            <div className={Styles.field}>
+                <div className={Styles.fieldsHeader}>
+                    <label htmlFor="title">Title</label>
+                </div>
+                <input 
+                    className={Styles.fieldInput} 
+                    name="title" 
+                    type="text" 
+                    placeholder="Ex: Market" 
+                    value={titleValue} 
+                    onChange={onChangeTitleValue}
+                />
+            </div>
+            <div className={Styles.field}>
+                <div className={Styles.fieldsHeader}>
+                    <label htmlFor="itens">Note Itens</label>
+                    <Button icon={Plus} onClick={onAddNewItem}>new item</Button>
+                </div>
+                <ul className={Styles.itemFieldsList}>
+                    {itensValue.map((item, index) => (
+                        <li key={`item-${index}`} className={Styles.fieldContainer}>
+                            <input 
+                                className={Styles.fieldInput}
+                                type="text" 
+                                placeholder="Ex: Buy milk" 
+                                value={item} 
+                                onChange={(event) => onChangeItemValue(event, index)}
+                            />
+                            {canDeleteItem ? <Trash onClick={() => onDeleteItem(index)}/> : null}
+                        </li>
+                    ))}
+                </ul>
+                <div className={Styles.actionsContainer}>
+                    <Button icon={Cancel} onClick={onCancel}>cancel</Button>
+                    <Button icon={Save} onClick={onSaveNote}>save note</Button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Form
--- src/presentation/pages/EditNote/index.tsx
+++ src/presentation/pages/EditNote/index.tsx
@@ -1,19 +1,158 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useNotes } from '@/presentation/hooks/useNotes'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import Styles from './styles.scss'
+import { Cancel, Plus, Save, Trash } from '@/presentation/icons'
+import { Button, ConfirmationModal } from '@/presentation/components'
+import { TodoItem } from '@/domain/models'
+import { useToggle } from '@/presentation/hooks'
+
+type ItensChangeHandler = (event: React.ChangeEvent<HTMLInputElement>, index: number) => void
 
 const EditNote: React.FC = () => {
     const { noteId } = useParams()
-    const { note, loadNote } = useNotes()
+    const navigate = useNavigate()
+    const { 
+        isActive: isConfirmDeleteOpen, 
+        enable: openConfirmDelete, 
+        disable: closeConfirmDelete 
+    } = useToggle()
+    
+    const { 
+        isActive: isConfirmResetOpen, 
+        enable: openConfirmReset, 
+        disable: closeConfirmReset 
+    } = useToggle()
+    
+    const { note, loadNote, deleteNote } = useNotes()
 
+    const [noteTitle, setNoteTitle] = useState('')
+    const [noteItens, setNoteItens] = useState<TodoItem[]>([])
 
     useEffect(() => {
-        loadNote({
+        const loadedNote = loadNote({
             id: noteId
         })
+
+        if(loadedNote) {
+            setNoteTitle(loadedNote.title)
+            setNoteItens(loadedNote.itens)
+        }
     }, [noteId])
 
-    return <h1>EditNote {noteId}</h1>
+    const canDeleteItem = noteItens.length > 1
+
+    const handleChangeTitle: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        const { value } = event.target
+        setNoteTitle(value)
+    }
+
+    const handleChangeItem: ItensChangeHandler = (event, id) => {
+        const { value } = event.target
+        const newItens = noteItens.map((item) => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    text: value
+                }
+            }
+
+            return item
+        })
+        setNoteItens(newItens)
+    }
+
+    const handleAddNewItem = () => {
+        setNoteItens(prev => [
+            ...prev,
+            { id: -1, isDone: false, text: '' }
+        ])
+    }
+
+    const handleDeleteNewItem = (id: number) => {
+        if(canDeleteItem) {
+            setNoteItens(prev => prev.filter((item) => item.id !== id))
+        }
+    }
+
+    const handleDeleteNote = useCallback(() => {
+        deleteNote({ id: note.id })
+    }, [])
+
+    const handleResetNote = useCallback(() => {
+        setNoteTitle(note.title)
+        setNoteItens(note.itens)
+        closeConfirmReset()
+    }, [note])
+
+    const handleSaveNote = () => {
+        console.log('edit note')
+    }
+
+    return (
+        <div className={Styles.editNotesContainer}>
+            <div className={Styles.editNotesWrapper}>
+                <div className={Styles.field}>
+                    <div className={Styles.fieldsHeader}>
+                        <label htmlFor="title">Title</label>
+                    </div>
+                    <input 
+                        className={Styles.fieldInput} 
+                        name="title" 
+                        type="text" 
+                        placeholder="Ex: Market" 
+                        value={noteTitle} 
+                        onChange={handleChangeTitle}
+                    />
+                </div>
+                <div className={Styles.field}>
+                    <div className={Styles.fieldsHeader}>
+                        <label htmlFor="itens">Note Itens</label>
+                    </div>
+                    <ul className={Styles.itemFieldsList}>
+                        {noteItens.map((item, index) => (
+                            <li key={`item-${index}`} className={Styles.fieldContainer}>
+                                <input 
+                                    className={Styles.fieldInput}
+                                    type="text" 
+                                    placeholder="Ex: Buy milk" 
+                                    value={item.text} 
+                                    onChange={(event) => handleChangeItem(event, index)}
+                                />
+                                {canDeleteItem ? <Trash className={Styles.delete} onClick={() => handleDeleteNewItem(index)}/> : null}
+                                {(noteItens.length - 1) === index ? <Plus className={Styles.add} onClick={handleAddNewItem} /> : null}
+                            </li>
+                        ))}
+                    </ul>
+                    <div className={Styles.actionsContainer}>
+                        <Button icon={Cancel} onClick={openConfirmReset}>reset changes</Button>
+                        <Button icon={Trash} onClick={openConfirmDelete}>delete note</Button>
+                        <Button icon={Save} onClick={handleSaveNote}>save note</Button>
+                    </div>
+                </div>
+            </div>
+            <ConfirmationModal 
+                title="Delete Confirmation" 
+                text={`
+                    Do you really desire to delete this note?
+                    Remember, if you delete this, all your tasks are going to be deleted too!   
+                `}
+                isOpen={isConfirmDeleteOpen} 
+                onClose={closeConfirmDelete}
+                onConfirm={handleDeleteNote}
+            />
+            <ConfirmationModal 
+                title="Reset Confirmation" 
+                text={`
+                    Do you really desire to reset your changes in this note?
+                    Remember, if you do this, all your changes are going to be lost!   
+                `}
+                isOpen={isConfirmResetOpen} 
+                onClose={closeConfirmReset}
+                onConfirm={handleResetNote}
+            />
+        </div>
+    )
 }
 
 export default EditNote
